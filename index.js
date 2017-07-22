48cde4249156c43ab281fe409c34d30aec88d8a2
Shi-Hao Hong <shihaohong94@gmail.com>
2017-07-21 19:16:47 -0700

    implement simple-peer video application

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+navigator.mediaDevices.getUserMedia({ video: true, audio: false})
+  .then((stream) => {
+    var Peer = require('simple-peer');
+    var peer = new Peer({
+      initiator: location.hash ==='#init', // sending out the signal
+      trickle: false, // ???
+      stream: stream
+    });
+
+    // creates a new peer event handler
+    peer.on('signal', function(data) {
+      document.getElementById('yourId').value = JSON.stringify(data);
+    });
+
+    document.getElementById('connect').addEventListener('click', function() {
+      var otherId = JSON.parse(document.getElementById('otherId').value);
+      peer.signal(otherId);
+    });
+
+    document.getElementById('send').addEventListener('click', function() {
+      var yourMessage = document.getElementById('yourMessage').value
+      peer.send(yourMessage);
+    });
+
+    peer.on('data', function(data) {
+      document.getElementById('messages').textContent += data + '\n';
+    });
+
+    peer.on('stream', function (stream) {
+      var video = document.createElement('video');
+      document.body.appendChild(video);
+
+      video.src = window.URL.createObjectURL(stream);
+      video.play();
+    })
+  })
+  .catch((err) => {
+    console.log(err);
+  });
